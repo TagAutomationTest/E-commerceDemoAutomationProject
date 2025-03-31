6a8dd0f04b59f7e426b9f7e1abf6d20e3d7e44d5
ashraf74 <ahmedashraaf09@gmail.com>
2025-04-01 01:30:22 +0200

    chron:adding aa new comment

--- src/main/java/com/swaglabs/drivers/AbstractDriver.java
+++ src/main/java/com/swaglabs/drivers/AbstractDriver.java
@@ -3,5 +3,6 @@ package com.swaglabs.drivers;
 import org.openqa.selenium.WebDriver;
 
 public abstract class AbstractDriver {
+    // This is an abstract class that defines a method to start a WebDriver instance.
     public abstract WebDriver startDriver();
 }
